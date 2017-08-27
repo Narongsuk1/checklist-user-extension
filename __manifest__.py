4135cf921d5b9bfcd3f3c66d891ed1f07f779b08
Narongsak Keawmanee <klogic@hotmail.co.th>
2017-08-27 22:01:46 +0700

    remove not usage code

--- __manifest__.py
+++ __manifest__.py
@@ -26,13 +26,6 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-        #'security/base_sequrity.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        #'demo/demo.xml',
     ],
 }
